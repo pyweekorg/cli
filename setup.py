462dd008145a68c2789f05399b85f6df9862f8c7
Daniel Pope <mauve@mauveweb.co.uk>
2019-04-05 09:39:03 +0100

    Set initial version number to 0.1.0

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(
     name='pyweek',
     short_description="CLI for Pyweek.",
+    version="0.1.0",
     author='Daniel Pope',
     author_email='mauve@mauveweb.co.uk',
     py_modules=['pyweek'],
